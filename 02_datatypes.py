b975073d6568e881520d1214f53f75ddc2c5018c
Arpita <arpitakumari10653@gmail.com>
2025-05-20 23:28:35 +0530

    know about variables

---
+++ 02_datatypes.py
@@ -0,0 +1,10 @@
+a = 1 # a is a integer 
+
+b = 2.2525 # b is a floating point number
+
+c = "Arpita" # c is a string 
+
+d = False # d is a boolean variable
+
+e = None # e is a none type variable
+
