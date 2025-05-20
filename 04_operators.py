0b49aee4032c802b5c802998382cd3f9264328dc
Arpita <arpitakumari10653@gmail.com>
2025-05-21 00:34:59 +0530

    add new operators use

--- 04_operators.py
+++ 04_operators.py
@@ -32,3 +32,5 @@ print("True and True is ", True and True)
 print("True and False is ", True and False)
 print("False and True is ", False and True)
 print("False and False is ", False and False)
+
+print(not(True))
