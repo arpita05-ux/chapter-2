a213118342a1152fdac20f730d22caadaf64addc
Arpita <arpitakumari10653@gmail.com>
2025-05-21 00:30:06 +0530

    operotors use

---
+++ 04_operators.py
@@ -0,0 +1,34 @@
+#Arithmetic operator
+a = 10
+b = 90
+c = a+b
+print(c)
+
+#Assingment operator
+a = 4-2 # Assing 4-2 in a
+print(a)
+b = 5
+b += 1 # Increment the value of b by 1 and then assign it to b
+print(b)
+c = 5
+c -= 1 # decrement the value of c by 1 and then assign it to c
+print(c)
+
+# Comparison operator
+d = 9<6 # result give in boolean 
+print(d)
+
+# Logical operator
+e = True or False
+print(e)
+#turth table of 'or'
+print("True or True is ", True or True)
+print("True or False is ", True or False)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+#turth table of 'and'
+print("True and True is ", True and True)
+print("True and False is ", True and False)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
