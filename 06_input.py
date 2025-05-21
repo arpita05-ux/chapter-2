aca63bb1bb82d282142d83aec45a2be95ee229b1
Arpita <arpitakumari10653@gmail.com>
2025-05-21 11:23:50 +0530

    change str into int and sum

--- 06_input.py
+++ 06_input.py
@@ -1,6 +1,6 @@
-a = input ("Enter number 1: ")
+a = int(input("Enter number 1: "))
  
-b = input ("Enter number 2: ")
+b = int(input("Enter number 2: "))
 
 print("Number a is : ", a)
 print("Number b is : ", b)
